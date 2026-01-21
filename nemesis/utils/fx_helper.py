0178e3dafba99fff0a23e47c3c76da3b733fdaca
Mxyzptlk-Z <ericcccc_z@outlook.com>
2026-01-21 14:34:36 +0800

    update fx helper

--- nemesis/utils/fx_helper.py
+++ nemesis/utils/fx_helper.py
@@ -2,6 +2,36 @@ import pandas as pd
 from .date import Date
 
 
+def get_fx_pair_base_size(fx_pair):
+    fx_pair_base_map = {
+        'USDJPY': 1e2,
+        'JPYUSD': 1e6,
+        'EURJPY': 1e2,
+        'JPYEUR': 1e6,
+        'CNYJPY': 1e2,
+        'JPYCNY': 1e6,
+        'CNHJPY': 1e2,
+        'JPYCNH': 1e6,
+        'USDIDR': 1,
+        'IDRUSD': 1e8,
+        'USDTHB': 1e2,
+        'THBUSD': 1e6,
+        'USDKRW': 1,
+        'KRWUSD': 1e8,
+        'USDTWD': 1,
+        'TWDUSD': 1e8,
+        'HKDJPY': 1e2,
+        'JPYHKD': 1e6,
+        'GBPJPY': 1e2,
+        'JPYGBP': 1e6,
+        'USDINR': 1e2,
+        'INRUSD': 1e6,
+    }
+    try:
+        return fx_pair_base_map[fx_pair]
+    except:
+        return 1e4
+
 def fx_ccy_trans(value, orig_ccy, fx_rate, fx_pair='None', vague_match_cny_cnh=False):
     if fx_pair == 'None' or fx_pair == 'NONE':
         return value
